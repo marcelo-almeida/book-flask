34a461b13f9991b832a718451dfb8461997cdceb
marcelo-almeida <marcelo777augusto@gmail.com>
2021-12-28 22:42:49 -0300

    adding routes.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, request
 from flask_sqlalchemy import SQLAlchemy
 
 from config import app_config, app_active
@@ -19,5 +19,34 @@ def create_app(config_name):
     @app.route('/')
     def index():
         return 'Hello World!'
+
+    @app.route('/login/')
+    def login():
+        return 'login'
+
+    @app.route('/recovery-password/')
+    def recovery_password():
+        return 'recovery password'
+
+    @app.route('/profile/<int:user_id>/action/<action>')
+    def profile(user_id, action):
+        if action == 'action1':
+            return f'Action1 for userId {user_id}'
+        elif action == 'action2':
+            return f'Action2 for userId {user_id}'
+        else:
+            return f'Action default for userId {user_id}'
+
+    @app.route('/profile/', methods=['POST'])
+    def create_profile():
+        username = request.form['username']
+        password = request.form['password']
+        return f'POST - username: {username}, password: {password}'
+
+    @app.route('/profile/<int:user_id>', methods=['PUT'])
+    def edit_total_profile(user_id):
+        username = request.form['username']
+        password = request.form['password']
+        return f'PUT - username: {username}, password: {password}'
 
     return app

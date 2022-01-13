3e2e627d3dd665f278dff0edb3e528f2016b891e
marcelo-almeida <marcelo777augusto@gmail.com>
2022-01-12 21:38:53 -0300

    adding jwt authentication

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask, request, redirect, render_template, Response, json
+from functools import wraps
+
+from flask import Flask, request, redirect, render_template, Response, json, abort
 from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 
@@ -29,6 +31,21 @@ def create_app(config_name):
         response.headers.add('Access-Control-Allow-Headers', 'Content-Type')
         response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTION')
         return response
+
+    def auth_token_required(f):
+        @wraps(f)
+        def verify_token(*args, **kwargs):
+            user = UserController()
+            try:
+                result = user.verify_auth_token(request.headers['access_token'])
+                if result['status'] == 200:
+                    return f(*args, **kwargs)
+                else:
+                    abort(result['status'], result['message'])
+            except KeyError as e:
+                abort(401, 'You need an access token')
+
+        return verify_token
 
     @app.route('/')
     def index():
@@ -77,27 +94,69 @@ def create_app(config_name):
 
     @app.route('/products', methods=['GET'])
     @app.route('/products/<limit>', methods=['GET'])
+    @auth_token_required
     def get_products(limit=None):
-        header = {}
+        header = {
+            'access_token': request.headers['access_token'],
+            'token_type': 'JWT'
+        }
         product = ProductController()
         response = product.get_products(limit=limit)
         return Response(
             json.dumps(response, ensure_ascii=False), mimetype='application/json'), response['status'], header
 
     @app.route('/product/<product_id>', methods=['GET'])
+    @auth_token_required
     def get_product(product_id):
-        header = {}
+        header = {
+            'access_token': request.headers['access_token'],
+            'token_type': 'JWT'
+        }
         product = ProductController()
         response = product.get_product_by_id(product_id=product_id)
         return Response(
             json.dumps(response, ensure_ascii=False), mimetype='application/json'), response['status'], header
 
     @app.route('/user/<user_id>', methods=['GET'])
+    @auth_token_required
     def get_user_profile(user_id):
-        header = {}
+        header = {
+            'access_token': request.headers['access_token'],
+            'token_type': 'JWT'
+        }
         user = UserController()
         response = user.get_user_by_id(user_id=user_id)
         return Response(
             json.dumps(response, ensure_ascii=False), mimetype='application/json'), response['status'], header
+
+    @app.route('/login_api/', methods=['POST'])
+    def login_api():
+        header = {}
+        user = UserController()
+        email = request.json['email']
+        password = request.json['password']
+        result = user.login(email=email, password=password)
+        code = 401
+        response = {
+            'message': 'user not authorized',
+            'result': []
+        }
+        if result:
+            if result.active:
+                result = {
+                    'id': result.id,
+                    'username': result.username,
+                    'email': result.email,
+                    'date_created': result.date_created,
+                    'active': result.active
+                }
+                header = {
+                    'access_token': user.generate_auth_token(result),
+                    'token_type': 'JWT'
+                }
+                code = 200
+                response['message'] = 'login authorized'
+                response['result'] = result
+        return Response(json.dumps(response, ensure_ascii=False), mimetype='application/json'), code, header
 
     return app
--- controller/user.py
+++ controller/user.py
@@ -1,4 +1,11 @@
+from datetime import datetime, timedelta
+
+import jwt
+
+from config import app_config, app_active
 from model.user import User
+
+config = app_config[app_active]
 
 
 class UserController:
@@ -37,3 +44,30 @@ class UserController:
                 'result': result,
                 'status': status
             }
+
+    def verify_auth_token(self, access_token):
+        status = 401
+        try:
+            jwt.decode(access_token, config.SECRET, algorithms='HS256')
+            message = 'Token is valid'
+            status = 200
+        except jwt.ExpiredSignatureError:
+            message = 'Token is expired'
+        except Exception:
+            message = 'Token is invalid'
+        return {
+            'message': message,
+            'status': status
+        }
+
+    def generate_auth_token(self, data, exp=30, time_exp=False):
+        if time_exp:
+            date_time = data['exp']
+        else:
+            date_time = datetime.utcnow() + timedelta(minutes=exp)
+        dict_jwt = {
+            'id': data['id'],
+            'username': data['username'],
+            'exp': date_time
+        }
+        return jwt.encode(dict_jwt, config.SECRET, algorithm='HS256')
